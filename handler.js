ec699b1f79c9b7a8fe01ec03329b22bc3a687ec1
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2019-06-30 17:40:11 +0900

    debug: add console

--- handler.js
+++ handler.js
@@ -6,7 +6,6 @@ import { app } from './app'
 const binaryMimeTypes = []
 const server = createServer(app, null, binaryMimeTypes)
 export const index = (event, context) => {
-  console.log('index')
   morgan.token('request-id', () => context.awsRequestId)
   return proxy(server, event, context)
 }
